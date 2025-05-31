902cf00a10f2075b5aa592e00d5453d3c63658ab
sadegh akbari <57003400+sadegh1379@users.noreply.github.com>
2025-05-31 10:25:09 +0330

    Update user.model.ts

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,20 +1,17 @@
 import mongoose, { Document, Schema } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
-// اینترفیس برای تایپ دادن به مدل
 interface IUser extends Document {
   email: string;
   password: string;
   comparePassword: (password: string) => Promise<boolean>;
 }
 
-// اسکیما و مدل کاربر
 const userSchema = new Schema<IUser>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-// هش کردن پسورد قبل از ذخیره در دیتابیس
 userSchema.pre<IUser>('save', async function (next) {
   if (!this.isModified('password')) return next();
 
@@ -23,7 +20,6 @@ userSchema.pre<IUser>('save', async function (next) {
   next();
 });
 
-// متد مقایسه پسورد
 userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
   return bcrypt.compare(password, this.password);
 };
